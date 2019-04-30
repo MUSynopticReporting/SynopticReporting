1dcd72a726f3a4ffc8fead11e856479c04ca98d1
MakesCents <jaredfunk28@gmail.com>
2019-04-30 16:00:56 -0500

    Added license notices

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿//This file is part of MUSynopticReporting.
+
+//MUSynopticReporting is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    MUSynopticReporting is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU Affero General Public License for more details.
+
+//    You should have received a copy of the GNU Affero General Public License
+//    along with MUSynopticReporting.If not, see<https://www.gnu.org/licenses/>.
+
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
